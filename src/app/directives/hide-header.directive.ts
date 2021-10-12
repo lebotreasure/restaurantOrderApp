f24581bc79c8b203fbfc17071a2d2121f5c9d838
academy_learner_DAMAC10 <academy_learner_damac10@academy-learner-DAMAC10s-MacBook-Air.local>
2021-10-12 11:46:06 +0200

    Fixing UI

--- src/app/directives/hide-header.directive.ts
+++ src/app/directives/hide-header.directive.ts
@@ -1,10 +1,39 @@
-import { Directive } from '@angular/core';
+import { Directive, AfterViewInit, HostListener, Input, Renderer2 } from '@angular/core';
+import { isPlatform, DomController } from '@ionic/angular';
 
 @Directive({
   selector: '[appHideHeader]'
 })
-export class HideHeaderDirective {
+export class HideHeaderDirective implements AfterViewInit{
+  @Input('appHideHeader') header: any;
+  private headerHeight = isPlatform('ios') ? 44 : 56;
+  private children: any;
 
-  constructor() { }
+  constructor(
+    private renderer: Renderer2,
+    private domCtrl: DomController
+  ) { }
+
+  ngAfterViewInit(): void {
+    this.header = this.header.el;
+    this.children = this.header.children;
+  }
+
+  @HostListener('ionScroll', ['$event']) onContentScroll($event: any) {
+    const scrollTop: number = $event.detail.scrollTop;
+    let newPosition = -scrollTop;
+
+    if (newPosition < -this.headerHeight) {
+      newPosition = -scrollTop;
+    }
+    let newOpacity = 1 - (newPosition / -this.headerHeight);
+
+    this.domCtrl.write(() => {
+      this.renderer.setStyle(this.header, 'top', newPosition + 'px');
+      for (let c of this.children) {
+        this.renderer.setStyle(c, 'opacity', newOpacity);
+      }
+    });
+  }
 
 }
